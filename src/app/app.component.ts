122063438ea56f7a6e20b7d80042dbc537f97cdf
Isaias Silva <luiz.silva@adtalem.com>
2018-04-30 15:44:37 -0300

    Github login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { environment } from './../environments/environment';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  user: Observable<firebase.User>;
+
+  constructor(public afAuth: AngularFireAuth){
+    this.user=this.afAuth.authState;
+  }
+  //title = 'app';
+
+  loginGithub(){
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GithubAuthProvider());
+  }
+
+  sair(){
+    this.afAuth.auth.signOut();
+  }
 }
